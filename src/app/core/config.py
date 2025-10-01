23ba6fed326ac14c1f29f1584135568e4b4e8e74
Artur G <arturguimaraes.sk8@gmail.com>
2025-10-01 11:02:03 -0300

    feat: fix config settings class

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,25 +1,21 @@
-"""Application configuration Settings"""
-
 from pathlib import Path
 from typing import List
-
 from dotenv import load_dotenv
 from pydantic import AnyHttpUrl, Field
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 env_file = Path(__file__).parent.parent.parent / ".env"
 if env_file.exists():
     load_dotenv(env_file)
 
 class Settings(BaseSettings):
-    # Project base settings
-    PROJECT_NAME: str = Field(default="Keep it Secret API", env="PROJECT_NAME")
-    DESCRIPTION: str = Field(default="An API for managing secrets securely.", env="DESCRIPTION")
-    VERSION: str = Field(default="1.0.0", env="VERSION")
-    API_V1_STR: str = Field(default="/api/v1", env="API_V1_STR")
+    model_config = SettingsConfigDict(env_file=env_file)
 
-    # CORS Settings
+    PROJECT_NAME: str = Field(default="Keep it Secret API", alias="PROJECT_NAME")
+    DESCRIPTION: str = Field(default="An API for managing secrets securely.", alias="DESCRIPTION")
+    VERSION: str = Field(default="1.0.0", alias="VERSION")
+    API_V1_STR: str = Field(default="/api/v1", alias="API_V1_STR")
+
     BACKEND_CORS_ORIGINS: List[str | AnyHttpUrl] = ["*"]
-
 
 settings = Settings()
